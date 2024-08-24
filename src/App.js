8bc2aa8948c896a9ef0fcade5b7c2fdb3b42242e
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-24 12:39:08 +0530

    worked on fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+
+
+
 function App() {
+  const [ipCity,setIpCity] = useState('');
+  const [cityWeather,setCityWeather] = useState({});
+  const [load,setLoad] = useState(false);
+
+
+  const fetchWeather = async()=>{
+    setLoad(true);
+    try{
+      let res = await fetch (`https://api.weatherapi.com/v1/current.json?Key=754b649a25af4eb09d865631240106&q=${ipCity}`);
+      let data = await res.json();
+      if(data.hasOwnProperty("current")){
+        setCityWeather(data);
+       
+      }else{
+        throw new Error("not found")
+      }
+    }catch(err){
+      alert("Failed to fetch weather data")
+    }
+    finally{
+      setLoad(false)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchDiv">
+        <div className="searchIp">
+          <input type="text" placeholder="Enter city name" value={ipCity} onChange={(e)=>setIpCity(e.target.value)}/>
+        </div>
+        <button onClick={fetchWeather}>Search</button>
+      </div>
+      <div className="displayDiv">
+        {
+        load?<h5>Loading data...</h5>:cityWeather.hasOwnProperty("current")?<><div className="card">
+          <p>Temperature</p>
+          <span>{cityWeather.current.temp_c}°C</span>
+        </div>
+        <div className="card">
+        <p>Humidity</p>
+        <span>{cityWeather.current.humidity}%</span>
+        </div>
+        <div className="card">
+        <p>Condition</p>
+        <span>{cityWeather.current.condition.text}</span>
+        </div>
+        <div className="card">
+        <p>Wind Speed</p>
+        <span>{cityWeather.current.wind_kph}kph</span>
+        </div></>:""
+     }
+      </div>
     </div>
   );
 }
